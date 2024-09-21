436dfbe0f0182e3844d3d98ae17fde7ce06b6469
João Paulo <moscardicommerce@gmail.com>
2024-09-20 23:49:55 -0300

    Resolução do desafio POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Iphone iphone1 = new Iphone("(11)94238-1503", "Iphone 15 Pro", "123456789", 64);
+Nokia nokia1 = new Nokia("(11)99891-5196", "Nokia Tijolão", "987654321", 28);
+
+nokia1.InstalarAplicativo("Android");
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+
+
+iphone1.InstalarAplicativo("Telegram");
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+
+
